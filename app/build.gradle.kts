de58184f84dc4a291f5f38327a150eb9403925d8
NicoRuge <132839415+NicoRuge@users.noreply.github.com>
2026-04-26 14:48:41 +0200

    - changed version name to 3.0 and versioncode to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.nruge.iceinfo"
         minSdk = 33
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.3"
+        versionCode = 4
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
